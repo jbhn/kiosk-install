a990aad4a800f6c4622a1cc37d0b3fca433ddccd
jbhn <jb@jbhn.pl>
2026-02-21 11:50:19 +0100

    clear

---
+++ files/run-kiosk.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xset -dpms     # disable DPMS (Energy Star) features.
+xset s off     # disable screen saver
+xset s noblank # don't blank the video device
+
+matchbox-window-manager -use_titlebar no &
+unclutter &    # hide X mouse cursor unless mouse activated
+
+chromium --display=:0 --kiosk --incognito --window-position=0,0 https://jbhn.pl/
